0f57d2457b994a62c84015a8dbac3cfcd86fe275
Adrian Petersson <adriantimothypetersson@gmail.com>
2021-05-10 18:21:45 +0200

    onclick fetch

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React from 'react'
 import cover from "../assets/missing_cover.png"
 import { motion } from "framer-motion"
 const Movie = ({movie}) => {
